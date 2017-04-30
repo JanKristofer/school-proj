e4bc71a715c1e7a6ccbd21aa4c073a1588efa4e6
JanKristofer <jankristofergarcia@gmail.com>
2017-05-01 02:34:14 +0800

    Add files via upload

---
+++ src/java/controller/AddWord.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import Dao.MVC;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.WordModel;
+/**
+ *
+ * @author Jan
+ */
+@WebServlet(name = "AddWord", urlPatterns = {"/AddWord"})
+public class AddWord extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+             //Step 1:  Get data from the form.
+           
+              String word = request.getParameter("w");
+              String wordType = request.getParameter("type");
+              String def = request.getParameter("def");
+              
+              //Step 2:  Transfer the data to the model/class.
+              WordModel words = new WordModel();
+              words.setWord(word);
+              words.setType(wordType);
+              words.setDef(def);
+    
+              //Step 3:  Save the data to the database.
+              String sql = "insert into entries values(?,?,?,default)";
+              int i = 0;
+              i = MVC.AddRecord(words, sql);
+
+              if (i !=0){
+                response.sendRedirect("SuccessAdd.html");
+              }
+              else
+                 response.sendRedirect("ErrorAdd.html"); 
+           
+            
+            
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
